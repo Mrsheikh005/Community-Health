8a7aa916ddf39088c1a5ba32dd6a5df49337e1be
ZunairPasha <96973013+ZunairPasha@users.noreply.github.com>
2022-02-23 12:49:46 +0500

    commit-rename

--- ios/CommunityHealth/AppDelegate.m
+++ ios/CommunityHealth/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"VBA"
+                                                   moduleName:@"CommunityHealth"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
